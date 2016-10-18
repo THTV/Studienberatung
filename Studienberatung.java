de27ad5a274f5fe136a6fac1398cf0cc9da8939c
Tony Van <tienhuytonny@yahoo.de>
2016-10-18 20:18:01 +0200

    abi-no

--- Studienberatung.java
+++ Studienberatung.java
@@ -3,5 +3,10 @@ public class Studienberatung {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Hast du Abitur?");
 		String answer = scanner.next();
+		
+		if(answer.equals("no") {
+			System.out.println("Hast du Fachabitur?");
+			String answer2 = scanner.next();
+		}
 	}
 }
