642a1d14f62de4fc3fa3e88c92e9321c2ec7a595
Tony Van <tienhuytonny@yahoo.de>
2016-10-18 19:45:52 +0200

    first question

--- Studienberatung.java
+++ Studienberatung.java
@@ -1,6 +1,7 @@
 public class Studienberatung {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+		System.out.println("Hast du Abitur?");
+		String answer = scanner.next();
 	}
 }
